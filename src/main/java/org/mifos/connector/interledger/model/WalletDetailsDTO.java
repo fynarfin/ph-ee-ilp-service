6f7c1e9f0cfd675678bd9eac702d15b5f83fe149
creation2703 <61195844+creation2703@users.noreply.github.com>
2024-07-10 16:25:16 +0530

    Add folder structure for the project.

---
+++ src/main/java/org/mifos/connector/interledger/model/WalletDetailsDTO.java
@@ -0,0 +1,15 @@
+package org.mifos.connector.interledger.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WalletDetailsDTO {
+    private String id;
+    private String publicName;
+    private String assetCode;
+    private int assetScale;
+    private String authServer;
+    private String resourceServer;
+}
